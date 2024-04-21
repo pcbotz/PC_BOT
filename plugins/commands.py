a9afa3c23470cabbe8a3c6a0a4ae85a9a21f9b1b
RolexPc <157006183+RolexPc@users.noreply.github.com>
2024-04-21 14:24:23 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -1547,8 +1547,8 @@ async def plans_cmd_handler(client, message):
     ]
     reply_markup = InlineKeyboardMarkup(btn)
     await message.reply_photo(
-        photo=PAYMENT_QR,
-        caption=PAYMENT_TEXT,
+        photo=GROUP_PIC,
+        caption=GROUP_TEXT,
         reply_markup=reply_markup
     )
     
