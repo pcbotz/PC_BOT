3e47ab6275b548d6e5fd7084e1a80990d14f3c40
RolexPc <157006183+RolexPc@users.noreply.github.com>
2024-04-21 10:36:11 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -293,7 +293,16 @@ async def start(client, message):
                                                             InlineKeyboardButton('🖥️ Watch online 🖥️', url=stream)]])  # web stream Link
                     )
                 if STREAM_MODE == True:
-                    
+                    button = [[
+                         InlineKeyboardButton("Eɴɢ", 'feng'),
+                         InlineKeyboardButton("Mᴀʟ", 'fmal'),
+                         InlineKeyboardButton("Tᴀᴍ", 'ftam'),
+                         InlineKeyboardButton("Hɪɴ", 'fhin')   
+                     ],[
+                         InlineKeyboardButton('🖥 𝗡𝗘𝗪 𝗢𝗧𝗧 𝗨𝗣𝗗𝗔𝗧𝗘𝗦 🖥', url=f'https://t.me/OTT_ARAKAL_THERAVAD_MOVIESS')
+                     ],[     
+                         InlineKeyboardButton('⭕️ 𝗚𝗘𝗧 𝗢𝗨𝗥 𝗖𝗛𝗔𝗡𝗡𝗘𝗟 𝗟𝗜𝗡𝗞𝗦 ⭕️', url="https://t.me/ARAKAL_THERAVAD_GROUP_LINKS"),
+                     ]]
                 else:
                      button = [[
                          InlineKeyboardButton("Eɴɢ", 'feng'),
