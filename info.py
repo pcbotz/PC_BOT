431a8c792e581ab16f0e59059fa726f118877920
PC <126058657+pcbotz@users.noreply.github.com>
2024-06-08 11:40:49 +0530

    Update info.py

--- info.py
+++ info.py
@@ -104,7 +104,7 @@ if 'DYNO' in environ:
 else:
     ON_HEROKU = False
 BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
-FQDN = str(getenv('FQDN', BIND_ADRESS)) if not ON_HEROKU or getenv('FQDN') else APP_NAME+'.herokuapp.com'
+FQDN = str(getenv('FQDN', BIND_ADRESS)) if not ON_HEROKU or getenv('FQDN') else APP_NAME+'https://pcautofilterbot-1500897d5898.herokuapp.com'
 URL = "https://pcautofilterbot-1500897d5898.herokuapp.com/".format(FQDN) if ON_HEROKU or NO_PORT else \
     "https://pcautofilterbot-1500897d5898.herokuapp.com/".format(FQDN, PORT)
 SLEEP_THRESHOLD = int(environ.get('SLEEP_THRESHOLD', '60'))
